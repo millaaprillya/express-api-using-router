b0013d5d6d84aeb5218dad2179d29c79bd7c202f
millaaprillya <miliaaprillya22@gmail.com>
2021-09-03 06:33:14 +0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const controllers = require("../controllers");
 const router = Router();
-router.get("/", (req, res) => res.send("This is root!"));
+router.get("/home", (req, res) => res.send("This is root!"));
 router.get("/getData", (req, res) => res.send("Berhasil"));
 module.exports = router;
 
